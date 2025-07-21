d25706cbf03842110adcb5e64746d55b8de8da00
greenfield01 <83402726+greenfield01@users.noreply.github.com>
2025-07-21 15:17:51 +0100

    Update solution.sh

--- bash/solution.sh
+++ bash/solution.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-add() {
-  echo $(($1 + $2))
+run_test() {
+  input1=$1
+  input2=$2
+  expected=$3
+  output=$(bash solution.sh $input1 $input2)
+
+  if [ "$output" == "$expected" ]; then
+    echo "Test passed"
+    return 0
+  else
+    echo "Test failed: expected $expected, got $output"
+    return 1
+  fi
 }
 
-add "$1" "$2"
+run_test 2 3 5
+run_test 7 8 15
+run_test 0 0 0
