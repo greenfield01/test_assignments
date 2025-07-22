b44c565779eb438c482bca2a86c89f8ab85fc389
greenfield01 <83402726+greenfield01@users.noreply.github.com>
2025-07-22 13:16:26 +0100

    Update solution.c

--- c/solution.c
+++ c/solution.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int num, sum = 0;
-    scanf("%d", &num);
-
-    while (num > 0) {
-        sum += num % 10;
-        num /= 10;
+    int n, sum = 0;
+    scanf("%d", &n);
+    while (n != 0) {
+        sum += n % 10;
+        n /= 10;
     }
-
     printf("%d\n", sum);
     return 0;
 }
