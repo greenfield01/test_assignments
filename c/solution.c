ab15e747ffed0aa8ea8ecabda12aea929617d214
greenfield01 <83402726+greenfield01@users.noreply.github.com>
2025-07-22 12:43:48 +0100

    Update solution.c

--- c/solution.c
+++ c/solution.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
-#include <string.h>
-
-void reverse(char *str) {
-    int len = strlen(str);
-    for (int i = 0; i < len / 2; ++i) {
-        char temp = str[i];
-        str[i] = str[len - 1 - i];
-        str[len - 1 - i] = temp;
-    }
-}
 
 int main() {
-    char str[100];
-    scanf("%s", str);  // reads one word (no spaces)
-    reverse(str);
-    printf("%s\n", str);
+    int num, sum = 0;
+    scanf("%d", &num);
+
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    printf("%d\n", sum);
     return 0;
 }
