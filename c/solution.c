7b0094a256c0a5de6a6db87e6def3e59a89851de
greenfield01 <83402726+greenfield01@users.noreply.github.com>
2025-07-22 12:10:54 +0100

    Update solution.c

--- c/solution.c
+++ c/solution.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int add(int a, int b) {
-    return a + b;
+void reverse(char *str) {
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; ++i) {
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
 }
 
 int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d\n", add(a, b));
+    char str[100];
+    scanf("%s", str);  // reads one word (no spaces)
+    reverse(str);
+    printf("%s\n", str);
     return 0;
 }
